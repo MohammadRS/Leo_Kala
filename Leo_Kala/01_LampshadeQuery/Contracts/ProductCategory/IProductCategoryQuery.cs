c58f5ce2771788aef4fae82e3d8e2ca0e7b65567
MohammadRS <79171635+MohammadRS@users.noreply.github.com>
2021-04-11 12:41:29 +0430

    Ui Slide,ProductCategory Implement

---
+++ Leo_Kala/01_LampshadeQuery/Contracts/ProductCategory/IProductCategoryQuery.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace _01_LampshadeQuery.Contracts.ProductCategory
+{
+    public interface IProductCategoryQuery
+    {
+        ProductCategoryQueryModel GetProductCategoryWithProducstsBy(string slug);
+        List<ProductCategoryQueryModel> GetProductCategories();
+        List<ProductCategoryQueryModel> GetProductCategoriesWithProducts();
+    }
+}
--- Leo_Kala/ServiceHost/obj/Debug/net5.0/ServiceHost.MvcApplicationPartsAssemblyInfo.cs
+++ Leo_Kala/ServiceHost/obj/Debug/net5.0/ServiceHost.MvcApplicationPartsAssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("01_LampshadeQuery")]
 [assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("0_Framework")]
 [assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("ShopManagement.Application.Contracts")]
 [assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("ShopManagement.Application")]
