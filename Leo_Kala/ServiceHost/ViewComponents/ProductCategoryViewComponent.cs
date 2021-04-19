481c9bd4c4021daf119a670d689c332085f37008
MohammadRS <79171635+MohammadRS@users.noreply.github.com>
2021-04-19 23:44:18 +0430

    ProductCategoryWithProduct Slide Ui Implement

---
+++ Leo_Kala/ServiceHost/ViewComponents/ProductCategoryViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using _01_LampshadeQuery.Contracts.ProductCategory;
+using _01_LampshadeQuery.Contracts.Slide;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServiceHost.ViewComponents
+{
+    public class ProductCategoryViewComponent : ViewComponent
+    {
+        private readonly IProductCategoryQuery _productCategoryQuery;
+        public ProductCategoryViewComponent(IProductCategoryQuery productCategoryQuery)
+        {
+            _productCategoryQuery = productCategoryQuery;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var productCategory = _productCategoryQuery.GetProductCategories();
+            return View(productCategory);
+        }
+    }
+}
---
+++ Leo_Kala/ServiceHost/ViewComponents/SliderProduct.cs
@@ -0,0 +1,17 @@
+﻿using _01_LampshadeQuery.Contracts.Slide;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServiceHost.ViewComponents
+{
+    public class SliderProductViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
