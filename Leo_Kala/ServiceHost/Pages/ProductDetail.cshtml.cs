f9d24766ccd298cbbdd9ce8973bb55d24d5a7b85
MohammadRS <79171635+MohammadRS@users.noreply.github.com>
2021-04-28 13:11:12 +0430

    ProductDetail Page Implement

--- Leo_Kala/ServiceHost/Pages/ProductDetail.cshtml.cs
+++ Leo_Kala/ServiceHost/Pages/ProductDetail.cshtml.cs
@@ -1,19 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using _01_LampshadeQuery.Contracts.Product;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace ServiceHost.Pages.Product
+namespace ServiceHost.Pages
 {
-    public class IndexModel : PageModel
+    public class ProductDetailModel : PageModel
     {
-        public ProductQueryModel Product;
         private readonly IProductQuery _productQuery;
 
-        public IndexModel(IProductQuery productQuery)
+        public ProductDetailModel(IProductQuery productQuery)
         {
             _productQuery = productQuery;
         }
 
+        public ProductQueryModel Product;
+        
         public void OnGet(string id)
         {
             Product = _productQuery.GetProductDetails(id);
